ca6fc5e6a21d1ca0332b2cc403f0a519a24099e6
yosrahelal <helal.yosra@gmail.com>
2017-11-17 13:05:52 +0100

    final version deployed, fix deployment error

--- audit-ETH/new_truffle_project/contracts/new_version_myTokenContract.sol
+++ audit-ETH/new_truffle_project/contracts/new_version_myTokenContract.sol
@@ -1,38 +1,26 @@
 pragma solidity ^0.4.17;
 
-contract kycOracle {
-    address public oracleAddress;
-    mapping (address => bool) public kycClearances;
+import './kycOracle.sol';
 
-    event kycCleared(
-    address indexed _clearedAddress,
-    uint clearanceTimestamp
-    );
+contract myTokenContract is kycOracle{
+    address owner;
+    // kycOracle n'est pas un modifier, je suis pas sûre que ça passera lors des tests
+    // Il faut prévoir de modifier kycOracle en tant que "modifier" et là un tel appel sera bon
+    // Sinon, initialiser l'adresse de l'oracle directement dans le constructeur myTokenContract()
+    // En ajoutant la ligne suivante : oracleAddress =  0x18AEf80Dc69E9dAC454E2383A10FF37F969bc945; après owner = msg.sender;
+    // On peut virer internal à mon avis car c'est un constructeur donc il est par défaut internal !!
 
-    event kycAddressDeposited(
-    address indexed _clearedAddress,
-    uint depositValue,
-    uint depositTimestamp
-    );
-
-    modifier onlyKycCleared {
-        require(kycClearances[msg.sender]);
-        _;
-    }
-    /*
-    function kycOracle(address _oracleAddress){
+    function myTokenContract(address _oracleAddress){
+        owner = msg.sender;
         oracleAddress = _oracleAddress;
+        /*
+        Je préfère la première version avec l'@ en param
+        oracleAddress = 0x18AEf80Dc69E9dAC454E2383A10FF37F969bc945
+        */
     }
-    /*
-     Remix warning msg
-     browser/oracle_interface.sol:26:33: use of "block.timestamp": "block.timestamp"
-     can be influenced by miners to a certain degree.
-     That means that a miner can "choose" the block.timestamp, to a certain degree,
-     to change the outcome of a transaction in the mined block.
-    */
-    function clearKyc(address _clearedAddress) public {
-        require(msg.sender == oracleAddress);
-        kycCleared(_clearedAddress, block.timestamp);
-        kycClearances[_clearedAddress] = true;
+
+    function() payable  onlyKycCleared{
+        // Token distribution logic
+        kycAddressDeposited(msg.sender, msg.value, block.timestamp);
     }
 }
