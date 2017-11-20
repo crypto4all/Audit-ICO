5a1818c5c399533f0667e3b2ffbcc7f7c9c903c2
yosrahelal <helal.yosra@gmail.com>
2017-11-20 16:38:38 +0100

    deployment file modif

--- audit-ETH/new_truffle_project/migrations/2_deploy_contracts.js
+++ audit-ETH/new_truffle_project/migrations/2_deploy_contracts.js
@@ -1,15 +1,24 @@
-var oracle = artifacts.require("./kycOracle.sol");
-var token = artifacts.require("./myTokenContract.sol");
+var oracle = artifacts.require("./new_version_kycOracle.sol");
+var token = artifacts.require("./new_version_myTokenContract.sol");
 
 module.exports = function(deployer, network){
+	var owner = web3.eth.accounts[0];
 
 	if (network = 'development') {
-		deployer.deploy(oracle);
-		deployer.deploy(token);
+		return deployer.deploy(oracle, { from: owner }).then(function(){
+            console.log("oracle address: " + oracle.address);
+            return deployer.deploy(token, oracle.address).then(function(){
+                console.log("token address: " + token.address);
+            });
+        });
 	}
 	if (network = 'ropsten') {
-    		deployer.deploy(oracle);
-    		deployer.deploy(token);
+    	return deployer.deploy(oracle, { from: owner }).then(function(){
+            console.log("oracle address: " + oracle.address);
+    		return deployer.deploy(token, oracle.address).then(function(){
+    		    console.log("token address: " + token.address);
+    		});
+        });
     }
 
 };
