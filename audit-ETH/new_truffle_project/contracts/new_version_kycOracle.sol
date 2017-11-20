406947167e17075005ac7d4c996d452b492d8fc4
yosrahelal <helal.yosra@gmail.com>
2017-11-20 16:38:17 +0100

    rename contracts

--- audit-ETH/new_truffle_project/contracts/new_version_kycOracle.sol
+++ audit-ETH/new_truffle_project/contracts/new_version_kycOracle.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.17;
 
-contract kycOracle {
+contract new_version_kycOracle {
     address public oracleAddress;
     mapping (address => bool) public kycClearances;
 
@@ -19,20 +19,7 @@ contract kycOracle {
         require(kycClearances[msg.sender]);
         _;
     }
-    /*
-    A virer !!!!
-    ça sert à rien et elle causait des problèmes
-    tant que oracleAddress est public on l'initialise directement sur myTokenContract
-    function kycOracle(address _oracleAddress){
-        oracleAddress = _oracleAddress;
-    }
-    /*
-     Remix warning msg
-     browser/oracle_interface.sol:26:33: use of "block.timestamp": "block.timestamp"
-     can be influenced by miners to a certain degree.
-     That means that a miner can "choose" the block.timestamp, to a certain degree,
-     to change the outcome of a transaction in the mined block.
-    */
+
     function clearKyc(address _clearedAddress) public {
         require(msg.sender == oracleAddress);
         kycCleared(_clearedAddress, block.timestamp);
--- audit-ETH/new_truffle_project/contracts/new_version_myTokenContract.sol
+++ audit-ETH/new_truffle_project/contracts/new_version_myTokenContract.sol
@@ -1,22 +1,13 @@
 pragma solidity ^0.4.17;
 
-import './kycOracle.sol';
+import './new_version_kycOracle.sol';
 
-contract myTokenContract is kycOracle{
+contract new_version_myTokenContract is new_version_kycOracle{
     address owner;
-    // kycOracle n'est pas un modifier, je suis pas sûre que ça passera lors des tests
-    // Il faut prévoir de modifier kycOracle en tant que "modifier" et là un tel appel sera bon
-    // Sinon, initialiser l'adresse de l'oracle directement dans le constructeur myTokenContract()
-    // En ajoutant la ligne suivante : oracleAddress =  0x18AEf80Dc69E9dAC454E2383A10FF37F969bc945; après owner = msg.sender;
-    // On peut virer internal à mon avis car c'est un constructeur donc il est par défaut internal !!
 
-    function myTokenContract(address _oracleAddress){
+    function new_version_myTokenContract(address _oracleAddress){
         owner = msg.sender;
         oracleAddress = _oracleAddress;
-        /*
-        Je préfère la première version avec l'@ en param
-        oracleAddress = 0x18AEf80Dc69E9dAC454E2383A10FF37F969bc945
-        */
     }
 
     function() payable  onlyKycCleared{
